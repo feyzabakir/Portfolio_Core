be8ea235751aa6a4d4b3d8f7b050dbde1450bf52
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-05 22:32:47 +0300

    Tema eklendi.

---
+++ Portfolio_Core/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Core.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
