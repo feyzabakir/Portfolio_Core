594ba9980a8d30b4f6b5fea5daffadcb99e5c3b6
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-08 14:43:13 +0300

    Admin Teması eklendi.

---
+++ Portfolio_Core/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Core.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Portfolio_Core/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Core.Controllers
+{
+    public class SkillController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
