69d93fccb28468996e3dbfac6994ea04a0e2357d
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-10 23:27:34 +0300

    Experience, Portfolio, Fluent Validation

---
+++ BusinessLayer/ValidationRules/PortfolioValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class PortfolioValidator: AbstractValidator<Portfolio>
+    {
+        public PortfolioValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Project name cannot be blank");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Image field cannot be left blank.");
+            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Image 2 field cannot be left blank");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price field cannot be empty");
+            RuleFor(x => x.Values).NotEmpty().WithMessage("Completion rate field cannot be left blank");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Project name must be at least 5 characters");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("The project name must be a maximum of 100 characters.");
+        }
+    }
+}
---
+++ Portfolio_Core/Controllers/ExperienceController.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Core.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Experience List";
+            ViewBag.v2 = "Experiences";
+            ViewBag.v3 = "Experience List";
+            var values = experienceManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            ViewBag.v1 = "Add Experience";
+            ViewBag.v2 = "Experiences";
+            ViewBag.v3 = "Add Experience";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience e)
+        {
+            experienceManager.TAdd(e);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = experienceManager.TGetByID(id);
+            experienceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditExperience(int id)
+        {
+            ViewBag.v1 = "Edit Experience";
+            ViewBag.v2 = "Experiences";
+            ViewBag.v3 = "Edit Experience";
+            var values = experienceManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditExperience(Experience e)
+        {
+            experienceManager.TUpdate(e);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
