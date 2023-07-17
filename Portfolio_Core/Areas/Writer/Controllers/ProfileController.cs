7e756553366e29310bc2b748af46941c0d0e897e
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-17 19:05:23 +0300

    Profile Page

---
+++ Portfolio_Core/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,59 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio_Core.Areas.Writer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Core.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+        [Route("Writer/[controller]/[action]")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.SurName;
+            model.PictureURL = values.ImageUrl;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                var stream = new FileStream(savelocation, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.SurName = p.Surname;
+            //user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
