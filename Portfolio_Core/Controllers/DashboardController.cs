6de56fdeafd0fb0e29d671581a40656690e12547
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-07-24 23:21:20 +0300

    admin panel top menu edited

--- Portfolio_Core/Controllers/DashboardController.cs
+++ Portfolio_Core/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Portfolio_Core.Controllers
 {
+    //[Authorize(Roles = "Admin")]
     public class DashboardController : Controller
       
     {
