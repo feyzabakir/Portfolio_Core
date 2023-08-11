54d692e543c906ac0216889fe04d25f7a79d4679
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-08-11 16:52:00 +0300

    revise

--- Portfolio_Core/Areas/Writer/Controllers/LoginController.cs
+++ Portfolio_Core/Areas/Writer/Controllers/LoginController.cs
@@ -34,7 +34,7 @@ namespace Core_Proje.Areas.Writer.Controllers
                 var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, true, true);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Profile", new { area = "Writer" });
+                    return RedirectToAction("Index", "Dashboard", new { area = "Writer" });
                 }
                 else
                 {
